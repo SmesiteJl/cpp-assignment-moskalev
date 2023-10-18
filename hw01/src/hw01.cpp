a04c875e81e77df61ad3bbae44b80f1c7cd5a696
Nikita <113058622+SmesiteJl@users.noreply.github.com>
2023-10-19 02:33:19 +0300

    hw01

---
+++ hw01/src/hw01.cpp
@@ -0,0 +1,42 @@
+#include <hw01.h>
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i <= std::sqrt(num); i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int sumOfDigits(int num) {
+    int sum = 0;
+
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+int hw01(int start, int end)
+{
+  if (start < 10 || start > 99 || end < 10 || end > 99) {
+        return -1;
+    }
+
+    int sum = 0;
+
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            sum += sumOfDigits(i);
+        }
+    }
+    return sum;
+}
